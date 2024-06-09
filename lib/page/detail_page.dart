64ab32faf2fd86b0d2b46bf2528dcdf5ce724e5b
acep nurman sidik <acepnurmansidik@gmail.com>
2024-06-09 15:50:30 +0700

    completed detail page

--- lib/page/detail_page.dart
+++ lib/page/detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:airplane/theme.dart';
+import 'package:airplane/widgets/custom_buttom.dart';
+import 'package:airplane/widgets/interest_item.dart';
+import 'package:airplane/widgets/photo_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -92,16 +95,157 @@ class DetailPage extends StatelessWidget {
                   ),
                 ],
               ),
+            ),
+
+            // NOTE: DESCRIPTION
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.only(top: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              decoration: BoxDecoration(
+                  color: kWhiteColor, borderRadius: BorderRadius.circular(18)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'About',
+                    style: blackTextStyle.copyWith(
+                        fontSize: 16, fontWeight: semibold),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Berada di jalur jalan provinsi yang menghubungkan Denpasar Singaraja serta letaknya yang dekat dengan Kebun Raya Eka Karya menjadikan tempat Bali.',
+                    style: blackTextStyle.copyWith(height: 1.6),
+                  ),
+
+                  // NOTE: PHOTOS
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Photos',
+                    style: blackTextStyle.copyWith(
+                        fontSize: 16, fontWeight: semibold),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        PhotoItem(
+                          imageUrl: 'assets/img_photo1.png',
+                        ),
+                        PhotoItem(
+                          imageUrl: 'assets/img_photo2.png',
+                        ),
+                        PhotoItem(
+                          imageUrl: 'assets/img_photo3.png',
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // NOTE: INTEREST
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Interests',
+                    style: blackTextStyle.copyWith(
+                        fontSize: 16, fontWeight: semibold),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      InterestItem(text: 'Kids Park'),
+                      InterestItem(text: 'Honor Bridge'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      InterestItem(text: 'City Museum'),
+                      InterestItem(text: 'Central Mall'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // PRICE & BOOK
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(vertical: 30),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'IDR 2.500.000',
+                          style: blackTextStyle.copyWith(
+                              fontSize: 18, fontWeight: medium),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('per orang', style: greyTextStyle),
+                      ],
+                    ),
+                  ),
+                  CustomButtom(
+                    title: 'Book Now',
+                    onPressed: () {},
+                    width: 170,
+                  )
+                ],
+              ),
             )
           ],
         ),
       );
     }
 
+    Widget buttonBack() {
+      return GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Container(
+          margin: EdgeInsets.only(top: 40, left: 24),
+          height: 40,
+          width: 40,
+          decoration: BoxDecoration(
+              color: kWhiteColor, borderRadius: BorderRadius.circular(50)),
+          child: Icon(
+            Icons.chevron_left,
+            color: kBlackColor,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
-      body: Stack(
-        children: [backgroundImage(), customShadow(), content()],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Stack(
+          children: [
+            backgroundImage(),
+            customShadow(),
+            content(),
+            buttonBack(),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/destination_tile.dart
+++ lib/widgets/destination_tile.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/page/detail_page.dart';
 import 'package:airplane/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,59 +17,65 @@ class DestinationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 16),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          color: kWhiteColor, borderRadius: BorderRadius.circular(18)),
-      child: Row(
-        children: [
-          Container(
-            height: 70,
-            width: 70,
-            margin: EdgeInsets.only(right: 16),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(18),
-                image: DecorationImage(
-                    image: AssetImage(imageUrl), fit: BoxFit.cover)),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style:
-                      blackTextStyle.copyWith(fontSize: 18, fontWeight: medium),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  city,
-                  style: greyTextStyle.copyWith(fontWeight: light),
-                ),
-              ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => DetailPage()));
+      },
+      child: Container(
+        margin: EdgeInsets.only(top: 16),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: kWhiteColor, borderRadius: BorderRadius.circular(18)),
+        child: Row(
+          children: [
+            Container(
+              height: 70,
+              width: 70,
+              margin: EdgeInsets.only(right: 16),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  image: DecorationImage(
+                      image: AssetImage(imageUrl), fit: BoxFit.cover)),
             ),
-          ),
-          Row(
-            children: [
-              Container(
-                height: 20,
-                width: 20,
-                margin: EdgeInsets.only(right: 2),
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage('assets/icon_star.png'))),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: blackTextStyle.copyWith(
+                        fontSize: 18, fontWeight: medium),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    city,
+                    style: greyTextStyle.copyWith(fontWeight: light),
+                  ),
+                ],
               ),
-              Text(
-                '$rating',
-                style: blackTextStyle.copyWith(fontWeight: medium),
-              )
-            ],
-          )
-        ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 20,
+                  width: 20,
+                  margin: EdgeInsets.only(right: 2),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/icon_star.png'))),
+                ),
+                Text(
+                  '$rating',
+                  style: blackTextStyle.copyWith(fontWeight: medium),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
