f27d046aaaf991e2d13ba804cd594344510c8353
acep nurman sidik <acepnurmansidik@gmail.com>
2024-06-18 20:55:02 +0700

    config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,16 @@ import 'package:airplane/page/get_started_page.dart';
 import 'package:airplane/page/main_page.dart';
 import 'package:airplane/page/signup_page.dart';
 import 'package:airplane/page/splash_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
